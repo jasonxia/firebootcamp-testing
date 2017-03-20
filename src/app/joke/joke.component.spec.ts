9246a358c54a04d4ae354ddd5726d20c011aa781
Duncan Hunter <duncan@duncanhunter.com.au>
2017-03-20 20:31:50 +1000

    add - isolated test example

--- src/app/joke/joke.component.spec.ts
+++ src/app/joke/joke.component.spec.ts
@@ -1,5 +1,11 @@
-describe(`Joke Component`, () => {
+import { JokeComponent } from './joke.component';
+describe(`Component: Joke Component`, () => {
     it(`should add 1 + 1`, () => {
         expect(1 + 1).toEqual(2);
+    });
+
+    it(`should have a title of 'Chuck Norris Jokes`, () => {
+        const component = new JokeComponent(null);
+        expect(component.title).toEqual('Chuck Norris Jokes');
     });
 });
