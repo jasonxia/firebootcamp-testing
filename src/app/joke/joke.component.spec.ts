e21520de4a94671d35522fc62f0a439e5a06219a
Duncan Hunter <duncan@duncanhunter.com.au>
2017-03-20 20:44:24 +1000

    add - testbed example

--- src/app/joke/joke.component.spec.ts
+++ src/app/joke/joke.component.spec.ts
@@ -1,24 +1,43 @@
 import { JokeComponent } from './joke.component';
+import { Component, DebugElement } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { JokeService } from './joke.service';
+import { By } from '@angular/platform-browser';
 describe(`Component: Joke Component`, () => {
+
+    let fixture: ComponentFixture<JokeComponent>;
     let component: JokeComponent;
     let jokeService: any;
+    let de: DebugElement;
 
     beforeEach(() => {
         jokeService = {
             getJoke: () => Observable.of('FAKE JOKE')
         };
 
-        component = new JokeComponent(jokeService);
+        TestBed.configureTestingModule({
+            declarations: [JokeComponent],
+            imports: [HttpModule],
+            providers: [
+                JokeService
+            ]
+        });
+
+        fixture = TestBed.createComponent(JokeComponent);
+        component = fixture.componentInstance;
+        jokeService = TestBed.get(JokeService);
+        de = fixture.debugElement;
     });
+
 
     it(`should add 1 + 1`, () => {
         expect(1 + 1).toEqual(2);
     });
 
-    it(`should have a title of 'Chuck Norris Jokes`, () => {
+    it(`should have a title of 'Chuck Norris quotes`, () => {
         expect(component.title).toEqual('Chuck Norris Jokes');
     });
 
@@ -26,9 +45,19 @@ describe(`Component: Joke Component`, () => {
         spyOn(jokeService, 'getJoke')
             .and.returnValue(Observable.of('FAKE JOKE'));
 
-        component.ngOnInit();
+        fixture.detectChanges();
 
         expect(component.joke).toEqual('FAKE JOKE');
     });
 
+    it(`should have joke content bound onto the page`, () => {
+        spyOn(jokeService, 'getJoke')
+            .and.returnValue(Observable.of('FAKE JOKE'));
+
+        fixture.detectChanges();
+
+        let el = de.query(By.css('p')).nativeElement;
+
+        expect(el.textContent).toEqual('FAKE JOKE');
+    });
 });
